72a378c503ad3962a0c91d1f2b72ff4a455ce022
Chris Markle <markle976@gmail.com>
2013-01-17 13:38:08 -0500

    Added display and drop functionality

--- disco.js
+++ disco.js
@@ -16,5 +16,31 @@ Game.prototype.init = function() {
 	}
 }
 
+Game.prototype.dropPiece = function(player, col) {
+	for (var row = 0; row < ROWS; row++) {
+		if (this.board[row][col] === 0) {
+			this.board[row][col] = player
+			break
+		}
+	}
+}
+
+Game.prototype.display = function(callback) {
+	for (var row = ROWS; row >= 0 ; row--) {
+		console.log(this.board[row]);
+		// for (var col = 0; col < COLS; col++) {
+		// 	callback(this.board[row][col], row, col)
+		// }
+	}
+}
+
+// opponent should reduce set to cols, rows, diags where total < 0 for defence
+// opponent should reduce set to cols, rows, diags where total >= 0 for offence
+
+/////////////////////////////////////////////////////////////////////////
+
+
 var game = new Game()
-console.log(game.board)
+
+
+
